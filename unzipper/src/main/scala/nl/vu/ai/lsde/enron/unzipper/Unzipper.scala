d15d141eb8037c9ceac0a3754b13dde1b647fc08
Andrea Jemmett <andreajemmett@gmail.com>
2016-02-20 16:24:02 +0100

    use spark save method

--- unzipper/src/main/scala/nl/vu/ai/lsde/enron/unzipper/Unzipper.scala
+++ unzipper/src/main/scala/nl/vu/ai/lsde/enron/unzipper/Unzipper.scala
@@ -36,29 +36,21 @@ object Unzipper extends App {
         val zis: ZipInputStream = new ZipInputStream(stream.open)
         var ze: ZipEntry = zis.getNextEntry
 
-        val folder = new File(outDir)
-        if (!folder.exists()) {
-            folder.mkdir()
-        }
-
         while (ze != null) {
             val fileName = ze.getName
 
             if (fileName.contains(filter)) {
-                val newFile = new File(outDir + File.separator + fileName.split(filter)(1))
-                println(s"File unzip: ${newFile.getAbsoluteFile}")
-                if (!newFile.getParentFile.mkdirs()) {
-                    throw new IOException(s"Unable to create directories for file ${newFile.getAbsolutePath}")
-                }
+                val newFile = outDir + File.separator + fileName.split(filter)(1)
+                println(s"File unzip: $newFile")
 
-                val fos = new FileOutputStream(newFile)
+                var fileTxt = ""
                 var len: Int = zis.read(buffer)
                 while (len > 0) {
-                    fos.write(buffer, 0, len)
+                    fileTxt += buffer.map(_.toChar)
                     len = zis.read(buffer)
                 }
 
-                fos.close()
+                sc.parallelize(fileTxt).saveAsTextFile(newFile)
             }
 
             ze = zis.getNextEntry
