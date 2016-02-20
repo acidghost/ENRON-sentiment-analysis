d6dbef0669eab3322447f46227e2735d1e2d1e71
Andrea Jemmett <andreajemmett@gmail.com>
2016-02-20 17:33:04 +0100

    minor

--- unzipper/src/main/scala/nl/vu/ai/lsde/enron/unzipper/Unzipper.scala
+++ unzipper/src/main/scala/nl/vu/ai/lsde/enron/unzipper/Unzipper.scala
@@ -1,11 +1,10 @@
 package nl.vu.ai.lsde.enron.unzipper
 
-import java.io.{IOException, FileOutputStream, File}
+import java.io.File
 import java.util.zip.{ZipEntry, ZipInputStream}
 
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkConf
 import org.apache.spark.input.PortableDataStream
+import org.apache.spark.{SparkConf, SparkContext}
 
 object Unzipper extends App {
 
@@ -13,7 +12,7 @@ object Unzipper extends App {
     val conf = new SparkConf().setAppName(appName)
     val sc = new SparkContext(conf)
 
-    val outDir = "hdfs:///user/hannesm/lsde/enron/extracted_txt"
+    val outDir = "hdfs:///user/lsde03/enron/extracted_txt"
 
     sc.binaryFiles("hdfs:///user/hannesm/lsde/enron/*.zip").foreach { case (fileName, stream) =>
         val zipName = fileName.split('/').last.split(".zip")(0)
@@ -27,9 +26,7 @@ object Unzipper extends App {
         unzip(stream, outDir + File.separator + mailboxName)
     }
 
-    def unzip(stream: PortableDataStream,
-              outDir: String,
-              filter: String = "text_000/") = {
+    def unzip(stream: PortableDataStream, mailboxDir: String, filter: String = "text_000/") = {
 
         val buffer = new Array[Byte](1024)
 
@@ -40,17 +37,18 @@ object Unzipper extends App {
             val fileName = ze.getName
 
             if (fileName.contains(filter)) {
-                val newFile = outDir + File.separator + fileName.split(filter)(1)
+                val newFile = mailboxDir + File.separator + fileName.split(filter)(1)
                 println(s"File unzip: $newFile")
 
                 var fileTxt = ""
                 var len: Int = zis.read(buffer)
                 while (len > 0) {
-                    fileTxt += buffer.map(_.toChar)
+                    fileTxt += buffer.map(_.toChar).mkString
                     len = zis.read(buffer)
                 }
 
-                sc.parallelize(fileTxt).saveAsTextFile(newFile)
+                val txt = sc.parallelize(fileTxt)
+                txt.saveAsTextFile(newFile)
             }
 
             ze = zis.getNextEntry
