5eecdda2a455002884c78cc41c9eb8e633f33a01
Andrea Jemmett <andreajemmett@gmail.com>
2016-02-21 11:11:26 +0100

    fixed errors in parser

--- unzipper/src/main/scala/nl/vu/ai/lsde/enron/unzipper/Parser.scala
+++ unzipper/src/main/scala/nl/vu/ai/lsde/enron/unzipper/Parser.scala
@@ -1,54 +1,46 @@
 package nl.vu.ai.lsde.enron.unzipper
 
-import java.io.File
-import java.util.zip.{ZipEntry, ZipInputStream}
-import scala.util.parsing.combinator.syntactical.StandardTokenParsers
-
-import org.apache.spark.input.PortableDataStream
 import org.apache.spark.{SparkConf, SparkContext}
 
-class Parser extends App{
+import scala.util.parsing.combinator.syntactical.StandardTokenParsers
 
-  class Email {
-    var date : String = _;
-    var from : String = _;
-    var to : String = _;
-    var cc : String = _;
-    var subject : String = _;
-    var body : String = _;
-  }
+class Parser extends App {
 
-  val filesDir = "hdfs:///user/lsde03/enron/extracted_txt"
-  val appName = "ENRON-parser"
-  val conf = new SparkConf().setAppName(appName)
-  val sc = new SparkContext(conf)
+    case class Email(date: String, from: String, to: String, cc: Option[String], subject: String, body: String)
 
-  val scalaParser = new StandardTokenParsers()
-  scalaParser.lexical.delimiters ++= List("\n")
-  scalaParser.lexical.reserved ++= List("Date:", "From:", "To:", "Cc:", "Subject:")
+    val filesDir = "hdfs:///user/lsde03/enron/extracted_txt"
+    val appName = "ENRON-parser"
+    val conf = new SparkConf().setAppName(appName)
+    val sc = new SparkContext(conf)
 
-  sc.wholeTextFiles(filesDir).foreach { case (fileName, fileContent) =>
+    val scalaParser = new StandardTokenParsers()
+    scalaParser.lexical.delimiters ++= List("\n")
+    scalaParser.lexical.reserved ++= List("Date:", "From:", "To:", "Cc:", "Subject:")
 
-    parse(fileContent)
-  }
+    sc.wholeTextFiles(filesDir).foreach { case (fileName, fileContent) =>
 
-  // parse text and returns Email obj
-  def parse(text: String) : Email = {
-    var scanner = new scalaParser.lexical.Scanner(text)
-    var email = new Email()
-    // TODO
-  }
+        parse(fileContent)
+    }
 
-  //  insert SparkSQL table and returns success code
-  def pushEntry(entry: Email) : Int = {
-    1
-  }
+    // parse text and returns Email obj
+    def parse(text: String): Email = {
+        // var scanner = new scalaParser.lexical.Scanner(text)
+        // var email = Email
+        // TODO
+        Email("date", "from", "to", Some("cc"), "subj", "body")
+    }
+
+    //  insert SparkSQL table and returns success code
+    def pushEntry(entry: Email): Int = {
+        1
+    }
 
 }
 
 // run with:
 // scala-2.11 -cp unzipper/target/scala-2.10/unzipper_2.10-1.0.0.jar nl.vu.ai.lsde.enron.unzipper.test
 object test extends App {
+
     var test = "Date: Wed, 7 Feb 2001 14:11:00 -0800 (PST)\nFrom: Harry Arora\nSubject: Allan Sommer\nX-SDOC: 528063\nX-ZLID: zl-edrm-enron-v2-arora-h-914.eml\n\n\n\n***********\nEDRM Enron Email Data Set has been produced in EML, PST and NSF format by ZL Technologies, Inc. This Data Set is licensed under a Creative Commons Attribution 3.0 United States License <http://creativecommons.org/licenses/by/3.0/us/> . To provide attribution, please cite to \"ZL Technologies, Inc. (http://www.zlti.com).\"\n***********"
     var p = new Parser()
     p.parse(test)
