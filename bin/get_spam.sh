105b4d5722481e5bb3154924b4e6ff8f97bffa88
Andrea Jemmett <andreajemmett@gmail.com>
2016-02-23 18:56:02 +0100

    added script to download enron spam dataset

---
+++ bin/get_spam.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source_url=http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/
+
+if [ -d "enron-spam" ] ; then
+    echo "enron-spam folder already exists"
+    exit 1
+fi
+mkdir -p enron-spam
+
+cd enron-spam
+for i in 1 2 3 4 5 6 ; do
+    wget "http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron"${i}".tar.gz"
+done
+cd ..
