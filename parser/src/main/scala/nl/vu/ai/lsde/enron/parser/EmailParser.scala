2e464be6acd94a60dc9e024c734d0572a06f87d4
Andrea Jemmett <andreajemmett@gmail.com>
2016-02-23 16:41:44 +0100

    better email parsing and filtering & moved Test class

--- parser/src/main/scala/nl/vu/ai/lsde/enron/parser/EmailParser.scala
+++ parser/src/main/scala/nl/vu/ai/lsde/enron/parser/EmailParser.scala
@@ -7,13 +7,16 @@ object EmailParser {
 
     val datePattern = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z '('z')'")
     val enronDatasetFooter = "\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\nEDRM Enron Email Data Set has been produced in EML"
+    val forwardedBy = ("---------------------- Forwarded by ", "---------------------------")
+    val subjectFwd = "^F[Ww]: [\\w\\W\\s]*$"
+    val subjectReply = "^R[Ee]: [\\w\\W\\s]*$"
+    val originalMsg = ">?\\s? -----Original Message-----"
+
 
     def parse(text: String): Email = {
         val (headers, body) = text.split("\n\n") match {
             case s: Array[String] => (s.head.split("\n"), s.tail.mkString("\n\n"))
         }
-
-        val bodyNoFooter = body.split(enronDatasetFooter) match { case s: Array[String] => s.head }
 
         val date = filterHeader(headers, "Date: ") match {
             case Some(s) =>
@@ -35,7 +38,27 @@ object EmailParser {
         val subject = filterHeader(headers, "Subject: ")
         if (subject.isEmpty) throw new EmailParsingException(s"Unable to parse SUBJECT header in email:\n$text")
 
-        Email(date.get, from.get, to, cc, bcc, subject.get, bodyNoFooter)
+        val bodyNoFooter = body.split(enronDatasetFooter) match { case s: Array[String] => s.head }
+        val bodyNoFwd = bodyNoFooter.replaceAll(forwardedBy._1, "").replaceAll(forwardedBy._2, "")
+
+        val bodyNoOrig = if (subject.get.matches(subjectFwd)) {
+            bodyNoFwd.replaceAll(originalMsg, "")
+        } else {
+            try { bodyNoFwd.split(originalMsg).head }
+            catch { case e: NoSuchElementException => throw new EmailParsingException(s"Unable to process body of email:\n$text") }
+        }
+
+        val bodyNoHeaders = bodyNoOrig .split('\n').map(_
+          .replaceAll(">?\\s?From: [^\\n]*$", "")
+          .replaceAll(">?\\s?To: [^\\n]*$", "")
+          .replaceAll(">?\\s?[Cc]c: [^\\n]*$", "")
+          .replaceAll(">?\\s?[Bb]cc: [^\\n]*$", "")
+          .replaceAll(">?\\s?Subject: [^\\n]*$", "")
+          .replaceAll(">?\\s?Sent: [^\\n]*$", "")
+          .replaceAll(">?\\s?Sent by: [^\\n]*$", "")
+        ).mkString("\n")
+
+        Email(date.get, from.get, to, cc, bcc, subject.get, bodyNoHeaders)
     }
 
     private def filterHeader(headers: Seq[String], filter: String): Option[String] = {
