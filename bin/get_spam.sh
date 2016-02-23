854490307527c67e4c846ad98360c3718168370f
Andrea Jemmett <andreajemmett@gmail.com>
2016-02-23 23:24:22 +0100

    extract spam dataset

--- bin/get_spam.sh
+++ bin/get_spam.sh
@@ -11,5 +11,7 @@ mkdir -p enron-spam
 cd enron-spam
 for i in 1 2 3 4 5 6 ; do
     wget "http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron"${i}".tar.gz"
+    tar -xzvf "enron"${i}".tar.gz"
+    rm "enron"${i}".tar.gz"
 done
 cd ..
