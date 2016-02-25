2575d8308aae261a13bc7f6a8ee475435e20ebb1
Enrico Rotundo <enrico.rotundo@gmail.com>
2016-02-25 10:40:21 +0100

    parser App -> main

--- parser/src/main/scala/nl/vu/ai/lsde/enron/parser/ParserDriver.scala
+++ parser/src/main/scala/nl/vu/ai/lsde/enron/parser/ParserDriver.scala
@@ -6,32 +6,36 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{SQLContext, SaveMode}
 import org.apache.spark.{SparkConf, SparkContext}
 
-object ParserDriver extends App {
+object ParserDriver {
 
-    // USARE METODO MAIN E METTERE TUTTO IL CONTENUTO IN MAIN
+    // scalastyle:off
+    def main (args: Array[String]) {
+        
+        val appName = "ENRON-parser"
+        val conf = new SparkConf().setAppName(appName)
+        val sc = new SparkContext(conf)
 
-    val appName = "ENRON-parser"
-    val conf = new SparkConf().setAppName(appName)
-    val sc = new SparkContext(conf)
+        val allExtracted = sc.objectFile[(String, Seq[String])](Commons.ENRON_EXTRACTED_TXT)
 
-    val allExtracted = sc.objectFile[(String, Seq[String])](Commons.ENRON_EXTRACTED_TXT)
+        // caricare il file csv, useare COLLECT!!!!!!!!!!!!!!!!!!!!!!!!!!! poi usare quella variabile dentro a map
 
-    // caricare il file csv, useare COLLECT!!!!!!!!!!!!!!!!!!!!!!!!!!! poi usare quella variabile dentro a map
+        val allParsed: RDD[MailBox] = allExtracted.map { case (mailbox, emails) =>
+            val parsedEmails = emails flatMap { email =>
+                try Some(EmailParser.parse(email))
+                catch {
+                    case e: EmailParsingException => None
+                }
+            }
 
-    val allParsed: RDD[MailBox] = allExtracted.map { case (mailbox, emails) =>
-        val parsedEmails = emails flatMap { email =>
-            try Some(EmailParser.parse(email))
-            catch { case e: EmailParsingException => None }
+            MailBox(mailbox, parsedEmails)
         }
 
-        MailBox(mailbox, parsedEmails)
+
+        val sqlContext = new SQLContext(sc)
+        import sqlContext.implicits._
+
+        val df = allParsed.toDF()
+        df.write.mode(SaveMode.Overwrite).parquet(Commons.ENRON_DATAFRAME)
     }
-
-
-    val sqlContext = new SQLContext(sc)
-    import sqlContext.implicits._
-
-    val df = allParsed.toDF()
-    df.write.mode(SaveMode.Overwrite).parquet(Commons.ENRON_DATAFRAME)
 
 }
