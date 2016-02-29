2561cb6590ce291f55e78b65aacf7cd047fc8d1a
Enrico Rotundo <enrico.rotundo@gmail.com>
2016-02-29 19:39:53 +0100

    started sentiment-resumer

--- sentiment-resumer/src/main/scala/nl/vu/ai/lsde/enron/sentiment-resumer/SentimentResumerTest.scala
+++ sentiment-resumer/src/main/scala/nl/vu/ai/lsde/enron/sentiment-resumer/SentimentResumerTest.scala
@@ -1,20 +1,30 @@
 package nl.vu.ai.lsde.enron.unzipper
 
+import nl.vu.ai.lsde.enron.Commons
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkContext, SparkConf}
 
 
 /**
-  * This driver program is responsible ...
+  * This driver program is responsible to compute the overall
+  * sentiment on a user-defined time-unit basis (e.g. 1 day)
   */
 object SentimentResumerTest {
 
-    val appName = "ENRON-unzipper"
-//    val conf = new SparkConf().setAppName(appName)
-//    val sc = new SparkContext(conf)
+    val appName = "ENRON-sentiment-resumer"
+    val conf = new SparkConf().setAppName(appName)
+    val sc = new SparkContext(conf)
 
     def main (args: Array[String]): Unit = {
-        println("ciao")
+        val sqlContext = new SQLContext(sc)
+        import sqlContext.implicits._
 
+        // load sentiment dataframe
+        val dfSentiment = sqlContext.read.parquet(Commons.ENRON_SENTIMENT_DATAFRAME)
+
+        //TODO for each day compute overall sentiment
+
+        //TODO Save the timeseries as CSV
 
     }
 }
