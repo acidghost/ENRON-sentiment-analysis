163c0fe246d9504cd19f5ff6892c04f6f852bc35
Enrico Rotundo <enrico.rotundo@gmail.com>
2016-03-25 17:20:35 +0100

    uo

--- post-processing.py
+++ post-processing.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib
 
 matplotlib.rc('pdf', fonttype=42)
-matplotlib.rcParams.update({'font.size': 9})
+matplotlib.rcParams.update({'font.size': 12})
 
 data_path = "visualization/data/part-00000"
 
@@ -17,14 +17,14 @@ df = df.sort_index()
 df = df.resample("W").mean()
 df.close = df.close.interpolate(method='polynomial', order=4)
 df.sentiment = df.sentiment.interpolate(method='polynomial', order=4)
-
+df["stock"] = df.close
 
 print df
 print df.corr()
 df.to_json(data_path+"_postprocessed.json")
 
-fig = plt.figure(figsize=(6, 4),)
-ax = df.close.plot(legend=True)
+fig = plt.figure(figsize=(6, 4))
+ax = df.stock.plot(legend=True)
 ax.set_ylabel('stock price [$]')
 
 df.sentiment.plot(secondary_y=True, style='g', legend=True, mark_right=False)
@@ -37,3 +37,9 @@ ax.right_ax.set_ylim(1,3)
 # plt.legend()
 plt.tight_layout()
 plt.savefig("paper/imgs/sentiment_vs_stock.pdf")
+
+fig = plt.figure(figsize=(6, 4))
+ax = df.sentiment.plot(kind="hist")
+ax.set_xlabel('sentiment')
+plt.tight_layout()
+plt.savefig("paper/imgs/sentiment_dist_hist.pdf")
